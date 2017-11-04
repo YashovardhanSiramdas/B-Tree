629e69835ed8bac7120868d3fd7fecbb92743d76
Yashovardhan Siramdas <ced16i028@iiitdm.ac.in>
2017-11-04 16:04:22 +0530

    Modified Node Structure and finished basic insertion

--- B-Tree.c
+++ B-Tree.c
@@ -1,3 +1,4 @@
+
 /*
 Author:- Yashovardhan Siramdas & Nipun Bharti
 
@@ -5,16 +6,25 @@ Programming and Data Strucutres Assignment (Dr. V Masilamani)
 
 B-Tree(3,3)
 
-Insertion Algorithm:-
+B-Tree Properties:-
 
 (i) Root is either a leaf or has atleast 2 children
 (ii) All the interior nodes and root node should have atleast 2 children and at max 3 children
 (iii) Each Leaf node should have atleast 2 keys and at max 3 keys
 (iv) Length of all paths from root to leaves are the same
-(v) Each Node will be in the form l1,k1,l2,k2,l3
+(v) Each Node will be in the form l1,k1,l2,k2,l3,k3,k4,pt,size
 (vi) All the keys in l1 subtree are less than k1
 (vii) All the keys in l2 subtree are more than or equal to k1 and less than k2
 (viii) All the keys in l3 subtree are more than or equal to k2
+
+Multi Purpose Struct:-
+
+Type 1:- Node
+Type 2:-Bucket
+If type=1 k1,k2 are keys for Node
+If type=2 k1,k2,k3 are data inside bucket and k4 acts as buffer
+size indicates number of occupied keys in Node or Bucket
+pt links to parent of Node or Bucket
 
 */
 
@@ -24,30 +34,22 @@ Insertion Algorithm:-
 //Node Structure
 struct node
 {
-	void *l1;
+	int type;
+	struct node *l1;
 	int k1;
-	void *l2;
+	struct node *l2;
 	int k2;
-	void *l3;
-	void *pt;
+	struct node *l3;
+	int k3;
+	int k4;
+	struct node *pt;
+	int size;	
 
 };
 
-/*
-Bucket Structure:-
-
-1st integer
-2nd integer
-3rd integer
-Buffer integer
-Bucket Size
-*/
-
 
 struct node *root = NULL;
-int init_b[5],bucket[5];
-int Bucket[100][5];
-int bcntr=0;
+
 
 void swap(int* a, int* b)
 {
@@ -56,42 +58,6 @@ void swap(int* a, int* b)
     *b = t;
 }
 
-int partition (int arr[], int low, int high)
-{
-    int pivot = arr[high];   
-    int i = (low - 1);  
- 
-    for (int j = low; j <= high- 1; j++)
-    {
-        if (arr[j] <= pivot)
-        {
-            i++;   
-            swap(&arr[i], &arr[j]);
-        }
-    }
-    swap(&arr[i + 1], &arr[high]);
-    return (i + 1);
-}
-
-void quickSort(int arr[], int low, int high)
-{
-    if (low < high)
-    {
-        int pi = partition(arr, low, high);
-        quickSort(arr, low, pi - 1);
-        quickSort(arr, pi + 1, high);
-    }
-}
-
-void setBucket(int bucket[])
-{
-	int i;
-	for(i=0;i<4;i++)
-		bucket[i]=999;
-	bucket[4]=0;
-}
-
-
 void insert()
 {
 	int x;
@@ -99,31 +65,74 @@ void insert()
 	scanf("%d",&x);
 	if(root==NULL)
 	{
-		init_b[init_b[4]]=x;
-		init_b[4]++;
-		quickSort(init_b,0,3);
-		if(init_b[4]==4)
-		{
-			struct node *temp=(struct node *)malloc(sizeof(struct node));
-			root=temp;
-			//int bucket[5];
-			setBucket(bucket);
-			bucket[0]=init_b[2];
-			bucket[1]=init_b[3];
-			bucket[4]=2;
-			init_b[2]=init_b[3]=999;
-			init_b[4]=2;
-			temp->l1=init_b;
-			temp->l2=bucket;
-			temp->l3=NULL;
-			temp->k1=bucket[0];
-			temp->pt=NULL;
-
-		}
+		struct node *init=(struct node *)malloc(sizeof(struct node));
+		root=init;
+		init->type=2;
+		init->k1=x;
+		init->size=init->size+1;	
 	}
 	else
 	{
+		if(root->type==2)
+		{
+			if(root->size==1)
+			{
+				root->k2=x;
+				root->size=root->size+1;
+				if(root->k1 > root->k2)
+					swap(&(root->k1),&(root->k2));	
+			}
+			else if(root->size==2)
+			{
+				root->k3=x;
+				root->size=root->size+1;
+				if(root->k3<=root->k1)
+				{
+					swap(&(root->k1),&(root->k3));
+					swap(&(root->k3),&(root->k2));
+				}
+				else if(root->k3>root->k1 && root->k3<=root->k2)
+				{
+					swap(&(root->k3),&(root->k2));	
+				}
 
+			}
+			else if(root->size==3)
+			{
+				root->k4=x;
+				if(root->k4<=root->k1)
+				{
+					swap(&(root->k1),&(root->k4));
+					swap(&(root->k4),&(root->k3));
+					swap(&(root->k2),&(root->k3));
+				}
+				else if(root->k4>root->k1 && root->k4<=root->k2)
+				{
+					swap(&(root->k2),&(root->k4));
+					swap(&(root->k3),&(root->k4));
+				}
+				else if(root->k4>root->k2 && root->k4<=root->k3)
+				{
+					swap(&(root->k3),&(root->k4));
+				}
+				struct node *sibling=(struct node *)malloc(sizeof(struct node));
+				struct node *parent=(struct node *)malloc(sizeof(struct node));
+				sibling->type=2;
+				parent->type=1;
+				sibling->k1=root->k3;
+				sibling->k2=root->k4;
+				root->k3=root->k4=0;
+				root->size=sibling->size=2;
+				root->pt=sibling->pt=parent;
+				parent->l1=root;
+				parent->l2=sibling;
+				root=parent;
+
+
+			}
+		}
+		else
+		{}
 	}
 	printf("%d is inserted into B-Tree\n",x);
 	
@@ -132,32 +141,14 @@ void insert()
 
 void display()
 {
-	int i;
-	if(root==NULL)
-	{
-		printf("b1\n");
-		for(i=0;i<init_b[4];i++)
-			printf("%d\n",init_b[i]);
-	}
-	else
-	{
-		printf("b1\n");
-		int *x=(int *)root->l1;
-		for(i=0;i<*(x+4);i++)
-			printf("%d\n",*(x+i));
-		printf("b2\n");
-		x=(int *)root->l2;
-		//printf("test %d\n",*(x+4));
-		for(i=0;i<*(x + 4);i++)
-			printf("%d\n",*(x+ i));	
-	}
+	
 }
 
 //Driver Function
 int main()
 {
 	int i;
-	setBucket(Bucket[0][5]);
+
 	int ch=0;
 	while(1)
 	{
@@ -177,7 +168,7 @@ int main()
 			}
 			case 3:
 			{
-				display();
+				//display();
 				break;
 			}
 			case 4:
